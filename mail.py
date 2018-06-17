4fc4bb488a833e9472cca1bbeca70627aec6c811
broholens <broholens@outlook.com>
2018-06-17 16:54:49 +0800

    use flask-restful

--- mail.py
+++ mail.py
@@ -1,31 +1,32 @@
-import os
-import zmail
-from flask import Flask, request
 from celery import Celery
+from flask import Flask, request
+from flask_restful import Api, Resource
+from mail_config import server_mapping_table
 
 REDIS_CONFIG = 'redis://localhost:6379'
-MAIL_SERVER = zmail.server(
-    os.environ.get('MAIL_USERNAME'), 
-    os.environ.get('MAIL_PASSWORD')
-)
 
 app = Flask(__name__)
 app.config['CELERY_BROKER_URL'] = REDIS_CONFIG
-# app.config['CELERY_RESULT_BACKEND'] = REDIS_CONFIG
- 
-celery = Celery(app.import_name, broker=REDIS_CONFIG)
+
+api = Api(app) 
+
+celery = Celery('mail', broker=REDIS_CONFIG)
 celery.conf.update(app.config)
 
-@celery.task
-def send_async_mail(mail):
-    to = mail.pop('to')
-    MAIL_SERVER.send_mail(to, mail)
+@celery.task()
+def send_email(mail):
+    recipient = mail.pop('recipient')
+    server_type = recipient.split('@')[-1]
+    server = server_mapping_table.get(server_type)
+    server.send_mail(recipient, mail)
 
-@app.route('/', methods=['POST'])
-def mail():
-    mail = request.form
-    send_async_mail.delay(mail)
-    return ''
+class Mail(Resource):
+    def post(self):
+        mail = request.form
+        send_email.delay(mail)
+        return '', 201
+
+api.add_resource(Mail, '/')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
